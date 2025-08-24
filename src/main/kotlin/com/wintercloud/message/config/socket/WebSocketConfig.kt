5d0da5c517442d796cda4c12241802893c9d06be
Tae Hee Jang <kancth03@gmail.com>
2025-08-24 22:32:57 +0900

    feat: websocket 연결용 config 구현

---
+++ src/main/kotlin/com/wintercloud/message/config/socket/WebSocketConfig.kt
@@ -0,0 +1,22 @@
+package com.wintercloud.message.config.socket
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig: WebSocketMessageBrokerConfigurer {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/connect")
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.setApplicationDestinationPrefixes("/app")
+        registry.enableSimpleBroker("/topic/room/")
+    }
+}
