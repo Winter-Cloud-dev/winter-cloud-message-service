319b03295ced43340a088758cd8e6eab9fd1024a
Tae Hee Jang <kancth03@gmail.com>
2025-08-24 22:32:14 +0900

    feat: kafka producer config 구현

---
+++ src/main/kotlin/com/wintercloud/message/config/kafka/KafkaProducerConfig.kt
@@ -0,0 +1,36 @@
+package com.wintercloud.message.config.kafka
+
+import com.wintercloud.message.entity.Message
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.serializer.JsonSerializer
+import java.io.Serializable
+import kotlin.jvm.java
+
+@EnableKafka
+@Configuration
+class KafkaProducerConfig {
+
+    @Value("\${spring.kafka.bootstrap-servers}")
+    lateinit var bootstrapServer: String
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Message> {
+        val factory = DefaultKafkaProducerFactory<String, Message>(producerConfigs())
+        return KafkaTemplate(factory)
+    }
+
+    @Bean
+    fun producerConfigs(): Map<String, Serializable> =
+        mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServer,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        )
+}
